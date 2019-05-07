562d73a8cd489b828f656f7d3b6abf7c2a9007b6
Igor Todorovic <igor333m@yahoo.com>
2019-05-07 09:01:35 -0700

    Add toggleTweet action

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,8 +1,32 @@
+import { saveLikeToggle } from '../utils/api';
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
+export const TOGGLE_TWEET = 'TOGGLE_TWEET';
 
 export function receiveTweets (tweets) {
   return {
     type: RECEIVE_TWEETS,
     tweets
+  }
+}
+
+function toggleTweet ({ id, authedUser, hasLiked }) {
+  return {
+    type: TOGGLE_TWEET,
+    id,
+    authedUser,
+    hasLiked
+  }
+}
+
+export function handleToggleTweet (info) {
+  return (dispach) => {
+    dispach(toggleTweet(info));
+
+    return saveLikeToggle(info)
+      .catch((e) => {
+        console.warn('Error in handleToggleTweet: ', e);
+        dispach(toggleTweet(info));
+        alert('There was an error liking this tweet, try again.');
+      });
   }
 }
