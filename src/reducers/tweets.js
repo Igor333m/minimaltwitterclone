cbe90762056005bf18f49c19e8f8a3eecbab340e
Igor Todorovic <igor333m@yahoo.com>
2019-05-15 09:01:09 -0700

    Add new tweet and reducer for it

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,4 +1,4 @@
-import { RECEIVE_TWEETS, TOGGLE_TWEET } from '../actions/tweets';
+import { RECEIVE_TWEETS, TOGGLE_TWEET, ADD_TWEET} from '../actions/tweets';
 
 
 export default function tweets (state = {}, action) {
@@ -17,6 +17,34 @@ export default function tweets (state = {}, action) {
             ? state[action.id].likes.filter( uid => uid !== action.authedUser )
             : state[action.id].likes.concat([action.authedUser])
         }
+      }
+      /**
+       * New Tweet Reducer.
+       * the new tweet needs to be added to the list of tweets
+       * an already existing tweet needs to be modified
+       *  if the new tweet is a response to another tweet
+       * In this reducer, we'll
+       * ***concatenate the new tweet to the list of the already-existing tweets.
+       *  Remember that the object spread operator offers us the most concise way of doing that;
+       * *** modify the replies property of the tweet the new tweet is replying to.
+       */
+    case ADD_TWEET :
+      const { tweet } = action;
+
+      let replyingTo = {};
+      if (tweet.replyingTo !== null) {
+        replyingTo = {
+          [tweet.replyingTo]: {
+            ...state[tweet.replyingTo],
+            replies: state[tweet.replyTo].replies.concat([tweet.id])
+          }
+        }
+      }
+
+      return {
+        ...state,
+        [action.tweet.id]: action.tweet,
+        ...replyingTo
       }
     default :
       return state
